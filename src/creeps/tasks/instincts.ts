b3a38070be7ba5c939c7f06e7cf945c23fe85415
Brian <contact@noimbrian.com>
2021-09-15 21:15:00 -0500

    Remove all the things

--- src/creeps/tasks/instincts.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @returns true when creep did work, false when creep did not
- */
-export const instincts = (creep: Creep): boolean => {
-    return false;
-};
--- src/utils/find.ts
+++
@@ -1,6 +0,0 @@
-export const findMaintainableBuildings = (room: Room) => room.find(
-    FIND_STRUCTURES,
-    {
-        filter: s => (s.hits < s.hitsMax / 2 && s.structureType !== "constructedWall") || s.hits < 15000,
-    },
-).sort((a, b) => (a.hits / a.hitsMax) - (b.hits / b.hitsMax));
