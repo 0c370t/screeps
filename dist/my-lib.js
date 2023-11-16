a18c96a3408ef2074f3c843d2d99bdd698e9e1b9
Brian D <contact@noimbrian.com>
2023-11-16 00:30:29 -0600

    chore: vite

---
+++ dist/my-lib.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const o=()=>{for(const[t,e]of Object.entries(Game.creeps))console.log(e.name)};exports.loop=o;
--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-var b=()=>{for(let[a,f]of Object.entries(Game.creeps))console.log(f.name)};export{b as loop};
+dist/my-lib.js
