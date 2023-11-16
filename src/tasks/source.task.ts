a0aeefcda849a94f53f3b9098516fdfdd73fa5d8
Brian D <contact@noimbrian.com>
2023-11-16 01:56:38 -0600

    updates

--- src/tasks/source.task.ts
+++ src/tasks/source.task.ts
@@ -5,7 +5,8 @@ export const sourceTask = (creep: Creep, target: Source) => {
   const r = creep.harvest(target);
   switch (r) {
     case ERR_NOT_IN_RANGE:
-      creep.moveTo(target, DEFAULT_MOVE_OPTS);
+      const moveResult = creep.moveTo(target, DEFAULT_MOVE_OPTS);
+      if (moveResult !== OK) creep.moveTo(target, Object.assign({}, DEFAULT_MOVE_OPTS, {reusePath: 0}))
       break;
     case OK:
       if (creep.store.getFreeCapacity() === 0) {
@@ -13,17 +14,18 @@ export const sourceTask = (creep: Creep, target: Source) => {
         if (!creep.room.controller)
           throw new Error("Creep is full in a room without a controller");
 
-        const spawns = creep.room.find(FIND_MY_SPAWNS, { filter: (s) => (s.store.getFreeCapacity(RESOURCE_ENERGY)) > 0 })
+        const spawns = creep.room.find(FIND_MY_SPAWNS, {
+          filter: (s) => s.store.getFreeCapacity(RESOURCE_ENERGY) > 0,
+        });
         if (spawns.length) {
-            finishTask(creep, spawns[0].id)
+          finishTask(creep, spawns[0].id);
         } else {
-            finishTask(creep, creep.room.controller.id)
+          finishTask(creep, creep.room.controller.id);
         }
       }
       break;
-      default:
-        creep.speak("Unhandled mining case", r)
-        break;
+    default:
+      creep.speak("Unhandled mining case", r);
+      break;
   }
-  
 };
