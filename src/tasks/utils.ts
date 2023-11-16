c99df17c801a1097693e164476c0666540bcc9cb
Brian D <contact@noimbrian.com>
2023-11-16 01:48:56 -0600

    updates

--- src/tasks/utils.ts
+++ src/tasks/utils.ts
@@ -1,7 +1,7 @@
 export const finishTask = (creep: Creep, nextId?: CreepMemory["task"]) => {
   creep.speak("Work Complete")
   if (creep.memory.task) {
-    const creepTaskIdx = creep.room.memory.tasks[creep.memory.task].indexOf(
+    const creepTaskIdx = creep.room.memory.tasks[creep.memory.task]?.indexOf(
       creep.name
     );
     creep.room.memory.tasks[creep.memory.task][creepTaskIdx] = null;
